ea71ae243ee5776f6ee3345ab077ce870fd9feb4
Victor Omenai <mediacinex@gmail.com>
2026-07-23 23:11:46 +0100

    chore: remove legacy frontend/, unused cinex-milestone-flow/, ignore .env.vercel

--- app/public/cinex-milestone-flow/server/assets/_tanstack-start-manifest_v-835jmdRE.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/ZBOOK 15 G5/cinex-milestone-flow/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BxgMQ0AX.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-BxgMQ0AX.js" } }] }, "/": { filePath: "C:/Users/ZBOOK 15 G5/cinex-milestone-flow/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CPmmDLYU.js"] } } });
-export {
-  tsrStartManifest
-};
